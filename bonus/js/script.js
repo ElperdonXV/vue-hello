7f065ff8f6f61cf6f25dbe74e2b7902fe6ea8e7b
ElperdonXV <davidemartinografico@gmail.com>
2021-12-15 15:22:23 +0100

    init bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,15 @@
+//BONUS 
+
+//Inseriamo un input per visualizzare in pagina un testo inserito da un utente.
+//Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
+//Poi aggiungiamo un pulsante per aumentare questo numero.
+
+const app = new Vue(
+    {
+        el: '#prova',
+        data: {
+            message: 'Provo ad inserire il testo tramite Vue-Js, che figata',
+            image: 'https://www.wikihow.com/images_en/thumb/2/29/Eat-Nutella-Step-1-Version-4.jpg/v4-460px-Eat-Nutella-Step-1-Version-4.jpg.webp'
+        }
+    }
+);
