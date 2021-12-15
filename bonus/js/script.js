65f35373bce3eab543efce622ea2589da820696a
ElperdonXV <davidemartinografico@gmail.com>
2021-12-15 15:25:50 +0100

    first bonus done

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -9,7 +9,9 @@ const app = new Vue(
         el: '#prova',
         data: {
             message: 'Provo ad inserire il testo tramite Vue-Js, che figata',
-            image: 'https://www.wikihow.com/images_en/thumb/2/29/Eat-Nutella-Step-1-Version-4.jpg/v4-460px-Eat-Nutella-Step-1-Version-4.jpg.webp'
+            image: 'https://www.wikihow.com/images_en/thumb/2/29/Eat-Nutella-Step-1-Version-4.jpg/v4-460px-Eat-Nutella-Step-1-Version-4.jpg.webp',
+            text: '',
+            placeholder: 'Inserisci qualcosa',
         }
     }
 );
